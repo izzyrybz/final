313864d0e253530fc85c9251396d044f1fda57f9
izzyrybz <isabelle.ry.96@gmail.com>
2023-03-22 10:39:04 +0100

    fixed the history generation

--- git_fetch_script.sh
+++ git_fetch_script.sh
@@ -7,14 +7,22 @@ if [ -z "$1" ]; then
 fi
 
 cwd=$(pwd)
-echo $cwd
+#echo $cwd
 cd "$1"
-echo "moved to the current directory $(pwd)"
+#echo "moved to the current directory $(pwd)"
 
 
 # Get the commit history with the desired format
 #log=$(git log --graph --pretty=format:'commit:%H,Author:%an,Date:%ad,Parents:%p%nChanged Files:%n' --name-status)
-log=$(git log --graph --boundary --pretty=format:'commit:%H,Author:%an,Date:%ad,Parents:%p%nChanged Files:%n' --name-status | sed -e 's/^commit/******&/')
+
+echo "fetching the git repository information"
+log=$(git log --graph --boundary --pretty=format:'%ncommit:%H,Author:%an,Date:%cd,Parents:%p%nChanged Files:%n' --name-status | sed -e 's/^commit/******&/')
 #echo $log
+
+#checking if the file is empty
+if [ -s $cwd/logfile.txt ]; then
+    #echo "File is not empty, clearing it..."
+    > $cwd/logfile.txt
+fi
 
 echo "$log" >> $cwd/logfile.txt
--- process_logfile.py
+++ process_logfile.py
@@ -5,6 +5,7 @@ import requests
 from pathlib import Path
 from datetime import datetime
 import copy
+from isodate import parse_datetime
 
 
     ###################################### TO CREATE THE COMMITS FROM THE TXT ################################################################
@@ -46,12 +47,14 @@ def info_between_elements(lst, A, B):
     #print(result)
     return result
 
+    ############################################ PROCESS THE INFORMATION FROM THE LOGFILE ###################################3
 
 def process_logfile(path):
     commit = {"commit_ref": "", "author": "", "date":"" ,"changed_files": [], "parents" :""}
     history = []
     date_format = "%a %b %d %H:%M:%S %Y %z"
     with open(path,'r') as f:
+        
         for line in f:
             line = line.strip()
             line = line.split(',')
@@ -63,25 +66,31 @@ def process_logfile(path):
                 if ('commit:' in element):
                     #print(element)
                     hash = element[element.index(":") + 1:]
+
                     commit["commit_ref"]  = hash
                 if('Author:' in element): 
                     #print("adding person",line[index+1])
                     name = element[element.index(":") + 1:].strip()
                     commit["author"] = name
-                '''if('Date:' in element):
+
+                if('Date:' in element):
                     date = element[element.index(":") + 1:]
-                    #print("THIS IS DATE:",date)
+                    #print(date)
                     date_obj = datetime.strptime(date, date_format)
-                    #print(date_obj)
-                    commit["date"] = date_obj'''
+                    date_string_with_t = date_obj.isoformat()
+                    #print(date_string_with_t)                    
+                    
+                    commit["date"] = date_string_with_t
+                
                 if('Parents:' in element): 
-                    print("parents",element, element[element.index(":") + 1:].strip())
+                    #print(element[element.index(":") + 1:].strip())
                     parents = element[element.index(":") + 1:].strip()
                     commit["parents"] = parents
+
                 if ('M' in element or 'D' in element or ' A ' in element or 'R100' in element):
                      
                     element = element.split()
-                    if(element[1] is 'R100'):
+                    if(element[1] == 'R100'):
                         files_and_action= [element[1],element[2],element[3]]
                         commit["changed_files"].append(files_and_action)
                     
@@ -91,13 +100,13 @@ def process_logfile(path):
                         #commit["changed_files"] = file_and_action
                         commit["changed_files"].append(file_and_action)
                     
-                    #commit['changed_files']+="${line}"$'\n'
-
                 if('*' in element):
+                    #print("now we are saving and")
                     #print("this is a ",commit)
                     history.append(commit)
                     commit = copy.deepcopy(commit)
-                            
+                    
+               
     #print(history)
     with open("history.txt", "w") as fp:
         for commit in history:
@@ -105,34 +114,53 @@ def process_logfile(path):
             fp.write('\n')
        
 
-    exit()
+    
     ############################################ TO CREATE THE RDF TRIPPLES #############################################
+    print("past the generation of the history")
+    #commits = URIRef("http://dbpedia.org/resource/Commit_(version_control)")
+    commits = URIRef("http://example.org/entity/commit")
 
-    commits = URIRef("http://dbpedia.org/resource/Commit_(version_control)")
-    # I think we can create namespace for like entity or ontology 
-    # https://dbpedia.org/ontology/author ???
-    #
+
     Author = URIRef("http://dbpedia.org/ontology/author")
-    Description = URIRef("http://dbpedia.org/ontology/description")
+    #Description = URIRef("http://dbpedia.org/ontology/description")
     Calendar_date = URIRef("http://dbpedia.org/ontology/Calendar_date")
+    Entity = URIRef("http://example.org/entity/")
 
+    #I am not sure if these are correct but we are working with it
+    Parent = URIRef("http://example.org/entity/parents")
+    Modify = URIRef("http://example.org/entity/modify")
+    Add = URIRef("http://example.org/entity/add")
+    Rename = URIRef("http://example.org/entity/rename")
 
     data=[]
     g = Graph()
 
     for commit in history:
-        #possible to make this https://github.com/izzyrybz/rdf_code_extended/commit+commit["commit_ref"] but we need to make sure of the uri for that
         urirefstring = "http://example.org/"+ commit['commit_ref']
         urirefstring= urirefstring.replace(" ", "")
         commit_uri = URIRef(urirefstring)
-        #commit_uri = commits[commit['commit_ref']]
         g.add((commit_uri, RDF.type, commits))
-        g.add((commit_uri, Author, Literal(commit['author'])))
-        g.add((commit_uri, Description, Literal(commit['description'])))
+        g.add((commit_uri, Author, Entity+commit['author']))
+        #only works when we print before??????
+        print(commit['date'])
         g.add((commit_uri, Calendar_date, Literal(commit['date'], datatype=XSD.dateTime)))
+        g.add((commit_uri, Parent,Entity+commit['parents'] ))
+        #if it has been renamed, there is two files
+        '''for action,file,_ in commit["changed_files"]:
+            if (action == 'M'):
+                #print("Modify") 
+                g.add((commit_uri, Modify, Entity+file))
+            elif (action == 'A'):
+                #print("Modify") 
+                g.add((commit_uri, Add, Entity+file))
+            elif (action == 'R100'):
+                #print("Modify") 
+                g.add((commit_uri, Rename, Entity+file))'''
+            
+
 
     # To save the graph to a file
-    g.serialize(destination='bigger_history.ttl', format='turtle')
+    g.serialize(destination='commit_history_turtle.ttl', format='turtle')
 
 
 
